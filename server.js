0273deee0f9e99fff1724e4a33765bdfdd0c19d1
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-11-08 00:34:02 +0800

    Testing an Express server with Postman

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Initialize middleware
+// we use to have to install body parser but now it is a build in middleware
+// function of express. It parsers incoming JSON payload
+app.use(express.json({ extended: false }));
 // just a test route for now
 app.get('/', (req, res) => res.send("Hello, World!"));
+app.post('/', (req, res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(8000, () => console.log(`Server started at port ${PORT}`));
