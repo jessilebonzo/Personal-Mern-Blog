19ece4645e31a763ab0411e1cf04312672a8df0b
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-11-13 11:07:52 +0800

    Adding comments functionality

--- server.js
+++ server.js
@@ -2,13 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const articlesInfo = {
+  "learn-tech": {
+    comments: [],
+  },
+  "read-books": {
+    comments: [],
+  },
+  "build-ai": {
+    comments: [],
+  },
+  "travel-tips": {
+    comments: [],
+  },
+};
 // Initialize middleware
 // we use to have to install body parser but now it is a build in middleware
 // function of express. It parsers incoming JSON payload
 app.use(express.json({ extended: false }));
-// just a test route for now
-app.get('/', (req, res) => res.send("Hello, World!"));
-app.post('/', (req, res) => res.send(`Hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+
+app.post('/api/articles/:name/add-comments', (req, res) => {
+  const {username, text} = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(8000, () => console.log(`Server started at port ${PORT}`));
